ccb88458633e0233648ae6752f25b5c1d53ee52f
BaptisteVandecrux <b.vandecrux@gmail.com>
2022-11-18 16:13:01 +0100

    new plots

--- GCN_positions_bav.py
+++ GCN_positions_bav.py
@@ -28,27 +28,20 @@ abc = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
 
 i=0
-for j in [1,2,3,4,5,6,7,8,9,10, 11,12,15,22,23,24]:
+for j in df_pos.id.unique():
 
-    if df_pos.loc[df_pos.id==j,'name'].iloc[0] in ['EGP', 'NEM', 'SDO', 'SDL', 'SUM', 'GITS']:
-       continue   
-    fig, ax=plt.subplots(1,3, figsize=(15,9))
-    plt.subplots_adjust(left=0.08, right=0.99,
-                        bottom=0.08,top=0.95,
-                        hspace = 0.35, wspace=0.25)
+    if df_pos.loc[df_pos.id==j,'name'].iloc[0] not in ['SWC', 'CP1', 'NAU', 
+                                                       'GIT', 'TUN', 'DY2', 
+                                                       'JR1', 'NAE','NSE', 'PET']:
+        continue
+    if j in [25, 26, 27, 28]:
+        continue
+    fig, ax=plt.subplots(1,1, figsize=(7,6))
     # ax=ax.flatten()
-    # ax = [ax]
+    ax = [ax]
     i = i+1
     tmp = df_pos.loc[df_pos.id==j,:].reset_index(drop=True)
 
-    ax[0].plot(tmp.lon, tmp.lat, marker = 'o', linestyle='None')
-    
-    texts = []
-    for k in tmp.index:
-        texts.append(ax[0].text(tmp.lon[k],
-                                tmp.lat[k], 
-                                tmp.date[k], fontsize=16,
-                                path_effects=[pe.withStroke(linewidth=4, foreground="white")]))
     ax[0].set_title(abc[i-1]+'. '+tmp.name_long.unique()[0])
 
     tmp_interp= pd.DataFrame()
@@ -64,23 +57,31 @@ for j in [1,2,3,4,5,6,7,8,9,10, 11,12,15,22,23,24]:
     tmp_interp = tmp_interp.reset_index()
     tmp_interp_y = tmp_interp_y.reset_index()
 
-    ax[0].plot(tmp_interp.lon, tmp_interp.lat, marker = '.', color='tab:red', linestyle='None')
-    ax[0].plot(tmp_interp_y.lon, tmp_interp_y.lat, marker = 'd',color='tab:red',  linestyle='None')
-    for k in tmp_interp_y.index:
-        texts.append(ax[0].text(tmp_interp_y.lon[k],
-                                tmp_interp_y.lat[k], 
-                                tmp_interp_y.date[k].year, fontsize=12))
-    # interpolating the x
-    ax[1].set_title('latitude vs time')
-    ax[1].plot(pd.to_datetime(tmp.date, errors='coerce'), tmp.lat, marker = 'o', linestyle='None')
-    ax[1].plot(tmp_interp.date, tmp_interp.lat, marker = '.', color='tab:red', linestyle='None')
-    ax[1].plot(tmp_interp_y.date, tmp_interp_y.lat, marker = 'd',color='tab:red',  linestyle='None')
+    # ax[0].plot(tmp_interp.lon, tmp_interp.lat, marker = '.', color='tab:red', linestyle='None')
+    ax[0].plot(tmp_interp_y.lon, tmp_interp_y.lat, 
+               label='annual inter- or extrapolated',
+               marker = 'd',color='tab:red',  linestyle='None')
     
-    # interpolating the y
-    ax[2].set_title('longitude vs time')
-    ax[2].plot(pd.to_datetime(tmp.date, errors='coerce'), tmp.lon, marker = 'o', linestyle='None')
-    ax[2].plot(tmp_interp.date, tmp_interp.lon, marker = '.', color='tab:red', linestyle='None')
-    ax[2].plot(tmp_interp_y.date, tmp_interp_y.lon, marker = 'd',color='tab:red',  linestyle='None')
+    for k in tmp_interp_y.index:
+        ax[0].text(tmp_interp_y.lon[k]+tmp_interp_y.lon.diff().abs().mean()*2,
+                tmp_interp_y.lat[k], 
+                tmp_interp_y.date[k].year, fontsize=11, color='lightgray')
+    ax[0].plot(tmp.lon, tmp.lat, 
+               label='observed',
+               marker = 'o', linestyle='None')
 
+    for k in tmp.index:
+        ax[0].text(tmp.lon[k]+tmp_interp_y.lon.diff().abs().mean()*2,
+                tmp.lat[k], 
+                tmp.date[k], fontsize=16,
+                path_effects=[pe.withStroke(linewidth=4, foreground="white")])
+    xlim = ax[0].get_xlim()
+    ax[0].set_xlim(xlim[0], xlim[1] + 0.2*(xlim[1] - xlim[0]))
+    plt.legend()
+    plt.xlabel('Longitude ($^o$E)')
+    plt.ylabel('Latitude ($^o$E)')
+    fig.savefig('figs/'+df_pos.loc[df_pos.id==j,'name'].iloc[0]+'.png')
+    print('![](https://github.com/GEUS-Glaciology-and-Climate/GCNet_positions/blob/main/figs/'+df_pos.loc[df_pos.id==j,'name'].iloc[0]+'.png)')
+    
 
         
